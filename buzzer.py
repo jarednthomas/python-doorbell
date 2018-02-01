471dfe2a19f0236b4b068d85f710d10d6946881c
Jared Thomas <jaredpyrothomas@gmail.com>
2018-02-01 15:37:29 -0600

    minor changes to whitespace and tabs

--- buzzer.py
+++ buzzer.py
@@ -5,19 +5,23 @@ doorbell
 
 Python based doorbell for the Raspberry Pi
 """
+
 import os
 import time
 import RPi.GPIO as GPIO
 import httplib, urllib
 from subprocess import call
 from datetime import datetime
+
 # pins (BCM) https://pinout.xyz/
 button = 4
 RED = 25
 GREEN = 24
 BLUE = 23
+
 # variables
 startRecordingMinSec = 1
+
 # GPIO setup
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -25,6 +29,7 @@ GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(RED, GPIO.OUT)
 GPIO.setup(GREEN, GPIO.OUT)
 GPIO.setup(BLUE, GPIO.OUT)
+
 # defines button press event
 buttonPressedTime = None
 def buttonStateChanged(pin):
@@ -66,15 +71,17 @@ def buttonStateChanged(pin):
             os.system('mpg123 -q /home/pi/Scripts/doorbell/sounds/doorbuzz.mp3 &')
         time.sleep(.2)
         GPIO.output(BLUE, False)
+
 try:
     # subscribe to button presses
     GPIO.add_event_detect(button, GPIO.BOTH, callback=buttonStateChanged)
     while True:
         # sleep to reduce CPU usage
         time.sleep(2)
+
 except KeyboardInterrupt:
     print("\n")
-    pass
+
 finally:
     GPIO.output(RED, False)
     GPIO.output(GREEN, False)
