ce9312afb85b3415840220d9cd77d8238edb81ce
Jared Thomas <jaredpyrothomas@gmail.com>
2018-02-01 15:26:12 -0600

    minor updates

--- buzzer.py
+++ buzzer.py
@@ -37,50 +37,49 @@ buttonPressedTime = None
 def buttonStateChanged(pin):
     global buttonPressedTime
     if not (GPIO.input(pin)):
-	# button is down
-	if buttonPressedTime is None:
-	    # reset LEDs
-	    GPIO.output(RED, False)
-	    GPIO.output(GREEN, False)
-	    GPIO.output(BLUE, False)
-	    buttonPressedTime = datetime.now()
+    # button is down
+    if buttonPressedTime is None:
+        # reset LEDs
+        GPIO.output(RED, False)
+        GPIO.output(GREEN, False)
+        GPIO.output(BLUE, False)
+        buttonPressedTime = datetime.now()
     else:
-	# button is up
-	if buttonPressedTime is not None:
+    # button is up
+    if buttonPressedTime is not None:
             elapsed = (datetime.now() - buttonPressedTime).total_seconds()
-	    buttonPressedTime = None
+        buttonPressedTime = None
 
-	    # button was held long enough
-	    if elapsed >= startRecordingMinSec:
+        # button was held long enough
+        if elapsed >= startRecordingMinSec:
 
-	        # prompt for recording input
-		# mp3 file and location will need to be updated based on your setup
-		GPIO.output(GREEN, True)
-		os.system('mpg123 -q /home/pi/Scripts/doorbell/sounds/whos_there.mp3 &')
-		time.sleep(1.25)
-		GPIO.output(GREEN,False)
+        # mp3 file and location will need to be updated based on your setup
+        GPIO.output(GREEN, True)
+        os.system('mpg123 -q /home/pi/Scripts/doorbell/sounds/whos_there.mp3 &')
+        time.sleep(1.25)
+        GPIO.output(GREEN,False)
 
-		# indicate recording with red
-		os.system('arecord -d 2 -f S16_LE -r 41000 ./sounds/Greeting.wav &')
-		GPIO.output(RED, True)
-		time.sleep(2)
-		GPIO.output(RED, False)
+        # indicate recording with red
+        os.system('arecord -d 2 -f S16_LE -r 41000 ./sounds/Greeting.wav &')
+        GPIO.output(RED, True)
+        time.sleep(2)
+        GPIO.output(RED, False)
 
-		# indicate when playing back with blue
-		GPIO.output(BLUE, True)
-		os.system('aplay -d 2 -f S16_LE -r 41000 ./sounds/Greeting.wav &')
-		time.sleep(2)
-		GPIO.output(BLUE, False)
+        # indicate when playing back with blue
+        GPIO.output(BLUE, True)
+        os.system('aplay -d 2 -f S16_LE -r 41000 ./sounds/Greeting.wav &')
+        time.sleep(2)
+        GPIO.output(BLUE, False)
 
-	    # button was not held long enough
-	    elif elapsed < startRecordingMinSec:
+        # button was not held long enough
+        elif elapsed < startRecordingMinSec:
 
-	        # play sound for regular doorbell
-		# mp3 file and location will need to be updated based on your setup
-		GPIO.output(BLUE, True)
-	        os.system('mpg123 -q /home/pi/Scripts/doorbell/sounds/doorbuzz.mp3 &')
-		time.sleep(.2)
-		GPIO.output(BLUE, False)
+        # play sound for regular doorbell
+
+        GPIO.output(BLUE, True)
+            os.system('mpg123 -q /home/pi/Scripts/doorbell/sounds/doorbuzz.mp3 &')
+        time.sleep(.2)
+        GPIO.output(BLUE, False)
 
 try:
     # subscribe to button presses
@@ -91,8 +90,8 @@ try:
         time.sleep(2)
 
 except KeyboardInterrupt:
-	print("\n")
-	pass
+    print("\n")
+    pass
 
 finally:
     GPIO.output(RED, False)
